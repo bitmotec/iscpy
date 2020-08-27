49b2f3d627586323dc28bacc2e6b4697a04b1d3f
averbeck <ghostcode1337@gmx.com>
2020-08-27 16:29:01 +0200

    Improved iscpy core in order to read dhcpd lease files

--- iscpy/iscpy_core/core.py
+++ iscpy/iscpy_core/core.py
@@ -28,30 +28,29 @@
 # OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-
 import copy
 import pickle
 
 
 def ParseTokens(char_list):
     """Parses exploded isc named.conf portions.
-    
+
     Inputs:
       char_list: List of isc file parts
-    
+
     Outputs:
       dict: fragment or full isc file dict
       Recursive dictionary of isc file, dict values can be of 3 types,
       dict, string and bool. Boolean values are always true. Booleans are false
       if key is absent. Booleans represent situations in isc files such as:
         acl "registered" { 10.1.0/32; 10.1.1:/32;}}
-    
+
       Example:
-    
+
       {'stanza1 "new"': 'test_info', 'stanza1 "embedded"': {'acl "registered"':
           {'10.1.0/32': True, '10.1.1/32': True}}}
     """
-    
+
     index = 0
     dictionary_fragment = {}
     new_char_list = copy.deepcopy(char_list)
@@ -65,6 +64,7 @@ def ParseTokens(char_list):
     key = 0
 
     while index < len(new_char_list):
+        # print("Line:", new_char_list[index])
         if new_char_list[index] == '{':
             last_open = index
         if new_char_list[index] == ';' and continuous_line:
@@ -73,14 +73,14 @@ def ParseTokens(char_list):
             continuous_line = False
         if new_char_list[index] == ';':
             continuous_line = False
-        if (    len(new_char_list) > index + 1 \
+        if (len(new_char_list) > index + 1 \
             and new_char_list[index] == '}' \
             and new_char_list[index + 1] != ';'
             ):
             skip, value = Clip(new_char_list[last_open:])
             temp_list.append({key: copy.deepcopy(ParseTokens(value))})
             continuous_line = True
-        if (    len(new_char_list) > index + 1 \
+        if (len(new_char_list) > index + 1 \
             and new_char_list[index + 1] == '{' \
             ):
             key = new_char_list.pop(index)
@@ -91,27 +91,29 @@ def ParseTokens(char_list):
                 dictionary_fragment[key] = copy.deepcopy(
                     ParseTokens(dict_value)
                     )
+            if key.startswith('lease'):
+                skip += 2
             index += skip
         else:
-            if (    len(new_char_list[index].split()) == 1 \
+            if (len(new_char_list[index].split()) == 1 \
                 and '{' not in new_char_list
                 ):
                 for item in new_char_list:
                     if item in [';']:
                         continue
                     dictionary_fragment[item] = True
-    
-            #If there are more than 1 'keywords' at new_char_list[index]
-            #ex - "recursion no;"
+
+            # If there are more than 1 'keywords' at new_char_list[index]
+            # ex - "recursion no;"
             elif len(new_char_list[index].split()) >= 2:
                 dictionary_fragment[new_char_list[index].split()[0]] = ' '.join(
                     new_char_list[index].split()[1:]
                     )
                 index += 1
-    
-            #If there is just 1 'keyword' at new_char_list[index]
-            #ex "recursion;" (not a valid option, but for example's sake it's fine)
-            elif( new_char_list[index] not in ['{', ';', '}'] ):
+
+            # If there is just 1 'keyword' at new_char_list[index]
+            # ex "recursion;" (not a valid option, but for example's sake it's fine)
+            elif(new_char_list[index] not in ['{', ';', '}']):
                 key = new_char_list[index]
                 dictionary_fragment[key] = ''
                 index += 1
@@ -119,23 +121,24 @@ def ParseTokens(char_list):
 
     return dictionary_fragment
 
+
 def Clip(char_list):
     """Clips char_list to individual stanza.
-    
+
     Inputs:
         char_list: partial of char_list from ParseTokens
-    
+
     Outputs:
         tuple: (int: skip to char list index, list: shortened char_list)
     """
-    
+
     assert char_list[0] == '{'
     char_list.pop(0)
     skip = 0
     for index, item in enumerate(char_list):
         if item == '{':
             skip += 1
-        elif (      item == '}' \
+        elif (item == '}' \
                 and skip == 0 \
             ):
             return (index, char_list[:index])
@@ -143,21 +146,49 @@ def Clip(char_list):
             skip -= 1
     raise Exception("Invalid brackets.")
 
+
 def Explode(isc_string):
     """Explodes isc file into relevant tokens.
-    
+
     Inputs:
         isc_string: String of isc file
-    
+
     Outputs:
         list: list of isc file tokens delimited by brackets and semicolons
             ['stanza1 "new"', '{', 'test_info', ';', '}']
     """
-    
+
     str_array = []
     temp_string = []
-    #prev_char = ''
+    bInSingleQuote = False
+    bInDoubleQuote = False
     for char in isc_string:
+        # Check for single quote
+        if bInSingleQuote and char == "'":
+            bInSingleQuote = False
+            temp_string.append(char)
+            continue
+        if not bInSingleQuote and not bInDoubleQuote and char == "'":
+            bInSingleQuote = True
+            temp_string.append(char)
+            continue
+        # Check for double quote
+        if bInDoubleQuote and char == '"':
+            bInDoubleQuote = False
+            temp_string.append(char)
+            continue
+        if not bInDoubleQuote and not bInSingleQuote and char == '"':
+            bInDoubleQuote = True
+            temp_string.append(char)
+            continue
+        # Keep quote
+        if bInSingleQuote or bInDoubleQuote:
+            if char in ['\n']:
+                temp_string.append(' ')
+                continue
+            temp_string.append(char)
+            continue
+        # Process char
         if char in ['\n']:
             continue
         if char in ['{', '}', ';']:
@@ -169,19 +200,19 @@ def Explode(isc_string):
                 temp_string = []
         else:
             temp_string.append(char)
-        #prev_char = char
     return str_array
+
 
 def ScrubComments(isc_string):
     """Clears comments from an isc file
-    
+
     Inputs:
         isc_string: string of isc file
-        
+
     Outputs:
         string: string of scrubbed isc file
     """
-    
+
     isc_list = []
     if isc_string is None:
         return ''
@@ -196,26 +227,26 @@ def ScrubComments(isc_string):
                 while True:
                     i, c = next(chars)
                     try:
-                        if (    c == '/' \
-                            and striped_line[i+1] == '*' \
+                        if (c == '/' \
+                            and striped_line[i + 1] == '*' \
                             ):
                             expanded_comment = True
-                            next(chars)  # Skip '*'
+                            next(chars) # Skip '*'
                             continue
-                        elif (      c == '*' \
-                                and striped_line[i+1] == '/' \
+                        elif (c == '*' \
+                                and striped_line[i + 1] == '/' \
                             ):
                             expanded_comment = False
-                            next(chars)  # Skip '/'
+                            next(chars) # Skip '/'
                             continue
-                        
+
                     except IndexError:
-                        continue  # We are at the end of the line
+                        continue # We are at the end of the line
                     if expanded_comment == True:
                         continue
                     else:
                         no_comment_line += c
-                        
+
             except StopIteration:
                 if no_comment_line:
                     isc_list.append(no_comment_line)
@@ -231,20 +262,80 @@ def ScrubComments(isc_string):
         if line.strip().startswith(('#', '//')):
             continue
         else:
-            isc_list.append(line.split('#')[0].split('//')[0].strip())
+            strTemp = ""
+            iQuoteStart, charQuote = _FindQuote(line)
+            if iQuoteStart > 0:
+                iPosition = 0
+                # Check first appering quote
+                while True:
+                    iQuoteStart = line[iPosition:].find(charQuote)
+                    if iQuoteStart >= 0:
+                        iQuoteStart += iPosition
+                        iQuoteEnd = line[iQuoteStart + 1:].find(charQuote) + iQuoteStart + 1
+                        try:
+                            if len(strTemp) == 0:
+                                strTemp += line[iPosition:iQuoteStart].split('#')[0].split('//')[0].lstrip()
+                            else:
+                                strTemp += line[iPosition:iQuoteStart].split('#')[0].split('//')[0]
+                        except ValueError:
+                            raise
+                            isc_list.append(line.split('#')[0].split('//')[0].strip())
+                            break
+                        strTemp += line[iQuoteStart:iQuoteEnd + 1]
+                        iPosition = iQuoteEnd + 1
+
+                    # Process string without quote or rest of string behind last quote
+                    else:
+                        iQuoteStart, charQuote = _FindQuote(line[iPosition:])
+                        if iQuoteStart < 0:
+                            if len(strTemp) > 0:
+                                isc_list.append(strTemp + line[iPosition:].split('#')[0].split('//')[0].rstrip())
+                                break
+                            isc_list.append(line[iPosition:].split('#')[0].split('//')[0].strip())
+                            break
+            else:
+                isc_list.append(line.split('#')[0].split('//')[0].strip())
+
     return '\n'.join(isc_list)
 
-def MakeISC(isc_dict, terminate=True):
+
+def _FindQuote(strLine):
+    """
+
+    """
+
+    # Check for quote starts
+    iQuoteStartSingle = strLine.find("'")
+    iQuoteStartDouble = strLine.find('"')
+    # Found both
+    if iQuoteStartSingle > 0 and iQuoteStartDouble > 0:
+        if iQuoteStartSingle < iQuoteStartDouble:
+            return iQuoteStartSingle, "'"
+        return iQuoteStartDouble, '"'
+    # Found single
+    if iQuoteStartSingle > 0:
+        return iQuoteStartSingle, "'"
+    # Found single
+    if iQuoteStartDouble > 0:
+        return iQuoteStartDouble, '"'
+    return -1, None
+
+
+def MakeISC(isc_dict, terminate=True, terminate_curly_brackets=True, end_with_newline=True):
     """Outputs an isc formatted file string from a dict
-    
+
     Inputs:
         isc_dict: a recursive dictionary to be turned into an isc file
                   (from ParseTokens)
-    
+        terminate: use semicolon to terminate all option lines
+        terminate_curly_brakets: use semicolon to terminate curly brackets
+                                 (if terminate is true)
+        end_with_newline:
+
     Outputs:
         str: string of isc file without indentation
     """
-    
+
     if terminate == True:
         terminator = ';'
     else:
@@ -254,75 +345,88 @@ def MakeISC(isc_dict, terminate=True):
     isc_list = []
     for option in isc_dict:
         if type(isc_dict[option]) == bool:
-            isc_list.append('%s%s' % (option, terminator))
-        elif ( type(isc_dict[option]) == str \
+            isc_list.append(f'{option}{terminator}')
+        elif (type(isc_dict[option]) == str \
                or type(isc_dict[option]) == str \
                ):
-            isc_list.append('%s %s%s' % (option, isc_dict[option], terminator))
+            isc_list.append(f'{option} {isc_dict[option]}{terminator}')
         elif type(isc_dict[option]) == list:
             new_list = []
             for item in isc_dict[option]:
-                new_list.append(MakeISC(item, terminate=False))
-            new_list[-1] = '%s%s' % (new_list[-1], terminator)
-            isc_list.append('%s { %s }%s' % (option, ' '.join(new_list), terminator))
+                new_list.append(MakeISC(item, terminate=False, end_with_newline=False))
+            new_list[-1] = f'{new_list[-1]}{terminator}'
+            if not terminate_curly_brackets:
+                isc_list.append(f'{option} {{ {" ".join(new_list)} }}')
+                continue
+            isc_list.append(f'{option} {{ {" ".join(new_list)} }}{terminator}')
         elif type(isc_dict[option]) == dict:
-            isc_list.append('%s { %s }%s' % (option, MakeISC(isc_dict[option]), terminator))
+            if not terminate_curly_brackets:
+                isc_list.append(f'{option} {{ {MakeISC(isc_dict[option], end_with_newline=False)} }}')
+                continue
+            isc_list.append(f'{option} {{ {MakeISC(isc_dict[option], end_with_newline=False)} }}{terminator}')
+    if end_with_newline:
+        return '\n'.join(isc_list) + '\n'
     return '\n'.join(isc_list)
+
 
 def ParseISCString(isc_string):
     """Makes a dictionary from an ISC file string
-    
+
     Inputs:
         isc_string: string of isc file
-    
+
     Outputs:
         dict: dictionary of ISC file representation
     """
-    
+
     return ParseTokens(Explode(ScrubComments(isc_string)))
+
 
 def Serialize(isc_string):
     """Makes a pickled byte string of a dict from an ISC file string
-    
+
     Inputs:
         isc_string: string of an isc file
-    
+
     Outputs:
         serialized_isc: serialized byte string of isc dict
     """
-    
+
     return pickle.dumps(ParseISCString(isc_string))
+
 
 def Deserialize(serialized_byte_string):
     """Makes an iscpy dict from a serliazed ISC dict
-    
+
     Inputs:
         serialized_byte_string: serialized byte string of isc dict
-    
+
     Outputs:
         deserialized_isc: unserialized dict of serialized isc dict
     """
-    
+
     return MakeISC(pickle.loads(serialized_byte_string))
+
 
 def AddZone(json_zone, isc_dict):
     """Add zone to named config
-    
+
     Inputs:
         json_zone: Zone definition in json format
-        
+
     Outputs:
         isc dict with added value
     """
-    
+
     isc_dict.update(json_zone)
     return isc_dict
 
+
 def ContentToWrite(isc_dict, num_tab, content, tokens):
     """Print ISC dictionary to specific file
-    
+
     """
-    
+
     s = ''
     for key, val in isc_dict.items():
         if key in tokens:
@@ -360,11 +464,12 @@ def ContentToWrite(isc_dict, num_tab, content, tokens):
     if num_tab == 0:
         return content
 
+
 def WriteToFile(isc_dict, isc_specialkeys, filename):
     """Write out the content of a ISC dictionary
-    
+
     """
-    
+
     with open(filename, "w") as f:
         conts = ContentToWrite(isc_dict, 0, [], isc_specialkeys)
         for c in conts:
